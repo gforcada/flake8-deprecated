cee6815e59ff35bb492a93936add1c8111610d57
Alex Muller <alex@mullr.net>
2017-10-22 17:43:43 +0200

    Use ast to parse the tree
    This has the benefit that we ensure it only checks actual function
    calls.
    
    As far as I can tell this means we don't need the filename at all
    anymore.
    
    The reason one of the tests has changed the column offset from 9 to
    0 is that the tree measures offsets in a different way to the string
    matching.
    
    I've followed the pattern from flake8-pytest quite closely in this
    commit:
    https://github.com/vikingco/flake8-pytest/blob/c6861968d7ee8fd9680343f6bae5fb/flake8_pytest.py

--- run_tests.py
+++ run_tests.py
@@ -1,45 +1,51 @@
 # -*- coding: utf-8 -*-
 from flake8_deprecated import Flake8Deprecated
-from tempfile import mkdtemp
 
-import os
+import ast
 import unittest
 
 
 class TestFlake8Deprecated(unittest.TestCase):
 
-    def _given_a_file_in_test_dir(self, contents):
-        test_dir = os.path.realpath(mkdtemp())
-        file_path = os.path.join(test_dir, 'test.py')
-        with open(file_path, 'w') as a_file:
-            a_file.write(contents)
-
-        return file_path
+    def _given_test_data(self, contents):
+        return ast.parse(contents)
 
     def test_no_deprecations_all_good(self):
-        file_path = self._given_a_file_in_test_dir('\n'.join([
+        tree = self._given_test_data('\n'.join([
             'b = 3',
             'b.lower()',
         ]))
-        checker = Flake8Deprecated(None, file_path)
+        checker = Flake8Deprecated(tree)
         ret = list(checker.run())
         self.assertEqual(len(ret), 0)
 
     def test_s_formatter(self):
-        file_path = self._given_a_file_in_test_dir('\n'.join([
+        tree = self._given_test_data('\n'.join([
             'import unittest',
             'unittest.failUnlessAlmostEqual()',
         ]))
-        checker = Flake8Deprecated(None, file_path)
+        checker = Flake8Deprecated(tree)
         ret = list(checker.run())
         self.assertEqual(len(ret), 1)
         self.assertEqual(ret[0][0], 2)
-        self.assertEqual(ret[0][1], 9)
+        self.assertEqual(ret[0][1], 0)
         self.assertEqual(
             ret[0][2],
-            'D001 found failUnlessAlmostEqual( replace it with '
+            'D001 found failUnlessAlmostEqual replace it with '
             'assertAlmostEqual'
         )
+
+    def test_ignores_comments(self):
+        tree = self._given_test_data('\n'.join([
+            'import unittest',
+            '# Maybe we could use assertEquals() here?',
+            'unittest.assertEquals()',
+        ]))
+        checker = Flake8Deprecated(tree)
+        ret = list(checker.run())
+        self.assertEqual(len(ret), 1)
+        lint_line_number = ret[0][0]
+        self.assertEqual(lint_line_number, 3)
 
 
 if __name__ == '__main__':
